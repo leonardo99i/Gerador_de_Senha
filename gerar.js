1841d79b122c62ac37918536343b1a64f44a884e
Leonardo Ribeiro Leonardi <49618344+Leo99z@users.noreply.github.com>
2021-06-30 00:23:59 -0300

    Realizando Manutenção e aprimorando o código

--- gerar.js
+++ gerar.js
@@ -1,11 +1,15 @@
 function geraSenha() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ.$%-_&!@+?:";
-    var passwordLength = 16;
-    var password = "";
+    var maiuscula = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
+    var minuscula = "abcdefghijklmnopqrstuvwxyz";
+    var numeros = "0123456789";
+    var simbolos = ".$%-_&!@+?:"
+    var juncao = maiuscula + minuscula + numeros + simbolos;
+    var Tamanho_Senha = 16;
+    var senha = "";
 
-    for (var i = 0; i < passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
+    for(var i = 0; i < Tamanho_Senha; i++) {
+      var randomNumber = Math.floor(Math.random() * juncao.length);
+      senha += juncao.substring(randomNumber, randomNumber + 1);
     }
-    document.getElementById('password').value = password
+    document.getElementById('password').value = senha;
   }
