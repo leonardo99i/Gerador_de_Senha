a6911aacfc54cc80416af3cd451d73a13143dc19
Leonardo Ribeiro Leonardi <49618344+Leo99z@users.noreply.github.com>
2021-06-30 01:28:24 -0300

    Update gerar.js

--- gerar.js
+++ gerar.js
@@ -1,4 +1,5 @@
 function geraSenha() {
+<<<<<<< Updated upstream
     var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*+?:";
     var passwordLength = 16;
     var password = "";
@@ -9,3 +10,19 @@ function geraSenha() {
     }
     document.getElementById('password').value = password
   }
+=======
+  var maiuscula = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
+  var minuscula = "abcdefghijklmnopqrstuvwxyz";
+  var numeros = "0123456789";
+  var simbolos = ".$%-_&!@+?:"
+  var juncao = maiuscula + minuscula + numeros + simbolos;
+  var Tamanho_Senha = 16;
+  var senha = "";
+
+  for (var i = 0; i < Tamanho_Senha; i++) {
+    var randomNumber = Math.floor(Math.random() * juncao.length);
+    senha += juncao.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById('password').value = senha;
+}
+>>>>>>> Stashed changes
