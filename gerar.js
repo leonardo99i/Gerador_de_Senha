d235b475af9dd7b727309ac22e91175c9450abc6
Leonardo Ribeiro Leonardi <49618344+Leo99z@users.noreply.github.com>
2021-07-01 18:17:35 -0300

    Atualização#1

--- gerar.js
+++ gerar.js
@@ -1,15 +1,16 @@
-function geraSenha() {
+function geraSenha(){
     var maiuscula = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
     var minuscula = "abcdefghijklmnopqrstuvwxyz";
     var numeros = "0123456789";
     var simbolos = ".$%-_&!@+?:"
     var juncao = maiuscula + minuscula + numeros + simbolos;
-    var Tamanho_Senha = 16;
+    var select = document.getElementById('escolha');
+    var value = select.options[select.selectedIndex].value;
     var senha = "";
-
-    for(var i = 0; i < Tamanho_Senha; i++) {
+    
+    for(var i = 0; i < value; i++) {
       var randomNumber = Math.floor(Math.random() * juncao.length);
       senha += juncao.substring(randomNumber, randomNumber + 1);
     }
     document.getElementById('password').value = senha;
-  }
+}
