da7351d48a6bba90dd8305ecb4c97bc0a4ee67b7
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-08-11 16:36:11 -0300

    Alguns Ajustes

--- gerar.js
+++ gerar.js
@@ -7,7 +7,7 @@ function geraSenha(){
     var select = document.getElementById('escolha');
     var value = select.options[select.selectedIndex].value;
     var senha = "";
-    
+        
     for(var i = 0; i < value; i++) {
       var randomNumber = Math.floor(Math.random() * juncao.length);
       senha += juncao.substring(randomNumber, randomNumber + 1);
