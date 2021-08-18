f6717bf3f7849ecb0932cd9006490a0c5978c4f3
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-08-18 00:24:49 -0300

    Tentando não errar

--- gerar.js
+++ gerar.js
@@ -1,16 +1,4 @@
 function geraSenha() {
-<<<<<<< Updated upstream
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*+?:";
-    var passwordLength = 16;
-    var password = "";
-
-    for (var i = 0; i < passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-    }
-    document.getElementById('password').value = password
-  }
-=======
   var maiuscula = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
   var minuscula = "abcdefghijklmnopqrstuvwxyz";
   var numeros = "0123456789";
@@ -25,4 +13,3 @@ function geraSenha() {
   }
   document.getElementById('password').value = senha;
 }
->>>>>>> Stashed changes
